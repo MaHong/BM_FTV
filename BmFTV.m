d8aea974aa290fda4eda40fd100b4f481f75f49b
MaHong <mamahong516@163.com>
2014-08-25 22:21:41 +0800

    调整刺激间隔，将学习及练习阶段注释。做了奇数被试，程序正确。

--- BmFTV.m
+++ BmFTV.m
@@ -1,17 +1,11 @@
 function BmFTV
-% function to test whether the memory load can influence the willness to
-% remember(distance judgement) or not
-
-
-
+% Test whether the memory load will influence FTA/FA judgement
 try
     % get basic subinfo
     [subID, name, sex, age] = tinputdialog;
-    
-    %  Setup the experiment
-    clear InputName MovieData
-        
+            
     %Screen setup %
+    clear InputName MovieData
     AssertOpenGL;
     InitializeMatlabOpenGL;
     PsychImaging('PrepareConfiguration');
@@ -28,43 +22,6 @@ try
     
     %3D set up
     threedparametersetupfun(w,wRect);
-%     Screen('BeginOpenGL',w);
-%     ar=RectHeight(wRect)/RectWidth(wRect);%屏幕高宽比
-%     % Set viewport properly:
-%     glViewport(0, 0, RectWidth(wRect), RectHeight(wRect));
-%     glColor3f(1,1,0);
-%     glEnable(GL.LIGHT0);
-%     glEnable(GL.BLEND);
-%     glBlendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
-%     glMatrixMode(GL.PROJECTION);
-%     glLoadIdentity;
-%     % Field of view is 25 degrees from line of sight. Objects closer than
-%     % 0.1 distance units or farther away than 100 distance units get clipped
-%     % away, aspect ratio is adapted to the monitors aspect ratio:
-%     gluPerspective(30, 1/ar, 0.1, 3000);%0.1和1000影响了相机设置的关系，离相机超过1000的东西都会被排除掉
-%     % Setup modelview matrix: This defines the position, orientation and
-%     % looking direction of the virtual camera:
-%     glMatrixMode(GL.MODELVIEW);
-%     glLoadIdentity;
-%     % Our point lightsource is at position (x,y,z) == (1,2,3)...
-%     glLightfv(GL.LIGHT0,GL.POSITION,[ 1 2 3 0 ]);%光源
-%     % Cam is located at 3D position (3,3,5), points upright (0,1,0) and fixates
-%     % at the origin (0,0,0) of the worlds coordinate system:
-%     % The OpenGL coordinate system is a right-handed system as follows:
-%     % Default origin is in the center of the display.
-%     % Positive x-Axis points horizontally to the right.
-%     % Positive y-Axis points vertically upwards.
-%     % Positive z-Axis points to the observer, perpendicular to the display
-%     % screens surface.
-%     gluLookAt(0,0,-1200,0,0,0,0,1,0);
-%     % Set background clear color to 'black' (R,G,B,A)=(0,0,0,0):
-%     glClearColor(0,0,0,0);
-%     % Clear out the backbuffer: This also cleans the depth-buffer for
-%     % proper occlusion handling: You need to glClear the depth buffer whenever
-%     % you redraw your scene, e.g., in an animation loop. Otherwise occlusion
-%     % handling will screw up in funny ways...
-%     glClear;
-%     Screen('EndOpenGL', w);
         
     % Screen priority
     priorityLevel=MaxPriority(w);
@@ -73,7 +30,7 @@ try
     % coordinates
     cm2pixel = 30; %convert centimeter to pixel
     % Rcircle=6 * cm2pixel; %the visual degress of the radius of the circle is 4 degree, which equal to 4.88 cm (distance to  the screen 70 cm)
-    spaceofstimulus = 8;
+    spaceofstimulus = 7;
     Rcircle=spaceofstimulus * cm2pixel;
     % positions
     n=1;
@@ -122,17 +79,17 @@ try
     %练习阶段指导语
     ShowInstruction(w,inssetup.start,inssetup.base,pos, mod(subID,2)==1);
     
-    % 学习FTV/FA
-    DisplayPLWalker(w,wRect,ftvparas,1,3000);
-    PushImages(w,pos,inssetup.FW);
-    ResponseforPLJudgment(keysetup.back);
-    DisplayPLWalker(w,wRect,ftvparas,20,4000);
-    PushImages(w,pos,inssetup.FTV);
-    ResponseforPLJudgment(keysetup.forward);
-    %练习阶段
-    PushImages(w,pos,inssetup.practiceStart);
-    WaitSecs(1.5);
-    StimulasInterval (w,1,frame_duration);
+%     学习FTV/FA
+%     DisplayPLWalker(w,wRect,ftvparas,1,3000);
+%     PushImages(w,pos,inssetup.FW);
+%     ResponseforPLJudgment(keysetup.back);
+%     DisplayPLWalker(w,wRect,ftvparas,20,4000);
+%     PushImages(w,pos,inssetup.FTV);
+%     ResponseforPLJudgment(keysetup.forward);
+%     练习阶段
+%     PushImages(w,pos,inssetup.practiceStart);
+%     WaitSecs(1.5);
+%     StimulasInterval (w,1,frame_duration);
 %     RunExperiment(w,wRect,24,frame_duration,...
 %     NumSplit,MovieCntre,act,ftvparas,inssetup,pos,...
 %     keysetup,subID,MovieFrames,a,b,12,1,inssetup.practiceOver);
